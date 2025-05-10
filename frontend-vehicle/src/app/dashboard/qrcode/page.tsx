c40880693b9b827e03e022627101c615bb9de14d
InduwaraSMPN <pasindunaduninduwara@gmail.com>
2025-05-11 00:11:25 +0530

    qrcode page added

---
+++ frontend-vehicle/src/app/dashboard/qrcode/page.tsx
@@ -0,0 +1,26 @@
+import { QRCodeGenerator } from "@/components/qr-code"
+import { ModeToggle } from "@/components/mode-toggle"
+import { Logo } from "@/components/logo"
+
+export default function Page() {
+  return (
+    <div className="flex flex-col min-h-svh w-full relative">
+      {/* Logo at the top */}
+      <div className="absolute top-0 left-0 z-10">
+        <Logo />
+      </div>
+
+      {/* Theme toggle button */}
+      <div className="fixed bottom-6 right-6 z-50">
+        <ModeToggle />
+      </div>
+
+      {/* Main content */}
+      <div className="flex flex-1 items-center justify-center p-6 md:p-10">
+        <div>
+            <QRCodeGenerator/>
+        </div>
+      </div>
+    </div>
+  )
+}
